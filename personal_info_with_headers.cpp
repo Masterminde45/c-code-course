9ea0dccee2dc8233a7c61be4f1fcee48fee3a28f
Masterminde45 <mats.pedersen2005@gmail.com>
2026-04-16 12:58:38 +0000

    added new file with footers and made this file an exe

---
+++ personal_info_with_headers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    // Display headers
+    std::cout << "====================================" << std::endl;
+    std::cout << "       PERSONAL INTRODUCTION        " <<std::endl ;
+    std::cout << "====================================" << std::endl;
+ 
+ 
+ // Diplay basic info
+ std::cout << "Name: Mats Pedersen" << std::endl;
+ std::cout << "Role: unemployed" << std::endl;
+ std::cout << "Hometown: Askim" << std::endl;
+ std::cout << "Hobby: gaming, building vms and some coding" << std::endl;
+ // Display footer
+ std::cout << "====================================" << std::endl;
+ return 0;
+
+
+  }
+
