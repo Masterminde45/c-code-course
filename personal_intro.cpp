5a41a95538589f38885e60d0ca82222ece52dfd4
Masterminde45 <mats.pedersen2005@gmail.com>
2026-04-16 11:53:41 +0000

    added full code

---
+++ personal_intro.cpp
@@ -0,0 +1,29 @@
+/*
+
+* Personal Introduction Program
+* Created by: Mats Pedersen
+*Date 04/14/2026
+*
+* Description: This program displays personal information 
+* about myself in a formated way on the console.
+*/
+
+/* 
+* My Learning Goals: 
+*
+*i hope to learn the basics of c++ programing which could be useful in the futuhre.
+ *
+ */
+
+// Include necessary header files
+#include <iostream>
+// Main function -program execution starts here
+int main() {
+// Display greeting with name
+std::cout << "Hello, my name is Mats Pedersen!" << std::endl;
+// Display current role/occupation
+std::cout << "I am currently unemployed,but i hope to become an it support agent one day." << std::endl;
+
+return 0;
+
+}
