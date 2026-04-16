105831f0613b9fd2abea046e6acee9a4e6dc8a6d
Mats Pedersen <mats.pedersen2005@gmail.com>
2026-04-16 13:23:18 +0200

    added existing files to repo for easy cross platform syncing

---
+++ c++ course/Helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Main function
+int main()
+{
+    // Output text
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ c++ course/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Setup Complete!" << endl;
+    return 0;
+}
