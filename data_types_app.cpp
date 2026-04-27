2b9ea27220131e80afe969f7b0f460ecdb885133
Masterminde45 <mats.pedersen2005@gmail.com>
2026-04-27 08:49:48 +0000

    made initial code in data_types_app.cpp and made it executable

---
+++ data_types_app.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <climits> //for data types limits like INT_MAX
+
+
+int main() {
+    // Display program header
+    std::cout << "=========================================" << std::endl;
+    std::cout << " PRACTICAL DATA TYPE APPLICATION         " << std::endl;
+    std::cout << "=========================================" << std::endl;
+    std::cout << "This program demonstrates  appropiate usage of different data types" << std::endl;
+    std::cout << "for  various kinds of information." << std::endl << std::endl;
+// Program sections will go here
+
+return 0;
+}
