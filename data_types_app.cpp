4ef99f4e689cd11cb0dc281d94162c736f50cf26
Masterminde45 <mats.pedersen2005@gmail.com>
2026-04-30 08:07:51 +0000

    added rest of the age section and updated the exe

--- data_types_app.cpp
+++ data_types_app.cpp
@@ -18,7 +18,21 @@ int main() {
     int teenAge = 15;
     int adultAge = 42;
 
+    // Display age information
+    std::cout << "Child age: " << childAge << " years" << std::endl;
+    std::cout << "Teen age: " << teenAge << " years" << std::endl;
+    std::cout << "Adult age: " << adultAge << " years" << std::endl;
+
+    // show memory usage
+    std::cout << "\nAge data memory usage: " <<std::endl;
+     std::cout << "size of int (for ages): " << sizeof(int) << "bytes" << std::endl;
     
+    // Demonstrate age calculations
+    std::cout << "\nAge calculations:" << std::endl;
+    int totalAges = childAge + teenAge + adultAge;
+    std::cout << "Total of all ages: " << totalAges << " years" << std::endl;
+
+
 
 
 return 0;
