42e2cc7688c168a6217687496dd7f2648df66821
Masterminde45 <mats.pedersen2005@gmail.com>
2026-04-30 07:40:03 +0000

    made integers and age data section for data types app

--- data_types_app.cpp
+++ data_types_app.cpp
@@ -9,7 +9,17 @@ int main() {
     std::cout << "=========================================" << std::endl;
     std::cout << "This program demonstrates  appropiate usage of different data types" << std::endl;
     std::cout << "for  various kinds of information." << std::endl << std::endl;
-// Program sections will go here
+// ------ AGE DATA SECTION -------
+    std::cout << "\n------ AGE DATA SECTION -------" <<std::endl;
+    std::cout << "selecting appropiate data types for age infotmation:" << std::endl;
+
+    // Integer type for ages (no fractional years needed)
+    int childAge = 8;
+    int teenAge = 15;
+    int adultAge = 42;
+
+    
+
 
 return 0;
 }
