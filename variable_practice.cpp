ca82b25569870f8db1439cd3fbf5cec56cd18838
Masterminde45 <mats.pedersen2005@gmail.com>
2026-05-04 12:28:15 +0000

    added palyerstats and made it executable

--- variable_practice.cpp
+++ variable_practice.cpp
@@ -5,11 +5,24 @@ int main() {
     double experiencePoints = 1250.75;
     char difficulityRating = 'B';
     bool hasCompletedTutoroial = true;
+    float Playerspeed = 20;
+    short playerlives = 4;
+    char playerGender = 'M';
+
     // Display the player information
     std::cout << "Player Profile:" <<std::endl;
     std::cout <<"Level: " << playerLevel << std::endl;
     std::cout << "Experience:" << experiencePoints << std::endl;
     std::cout << "Difficulity Rating: " << difficulityRating <<std::endl;
     std::cout << "Completed Tourtorial: " << hasCompletedTutoroial << std::endl;
+    std::cout << "Speed: " <<Playerspeed <<std::endl;
+    std::cout << "lives: " << playerlives <<std::endl;
+std::cout << "Gender: " << playerGender <<std::endl;
     return 0;
-}
+    }
+
+    #include <iostream>
+    int main(){
+    // Constants
+
+
