2e51884174a1e8fac71968fc8eecb1630964dc00
Masterminde45 <mats.pedersen2005@gmail.com>
2026-05-11 08:41:04 +0000

    made first code in calculator and let the calculation linger with an issue intentionaly

---
+++ Calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    // Integer division example
+    int totalPoints = 100;
+    int numTests = 3;
+    double average1 = totalPoints / numTests; // what happens here?
+    cout << "Average (implicit): " << average1 << endl;
+    // Character to integer conversion example
+    char grade = 'B';
+    int gradeValue = grade; // ASCII conversion
+    cout << "ASCII value of 'B' :" << gradeValue << endl;
+
+
+
+}
