c4cd217950ecbf2263da741629216d22797cf999
Masterminde45 <mats.pedersen2005@gmail.com>
2026-05-26 11:36:37 +0000

    prepared headers and start of the expression evaluator program

---
+++ expression_evaluator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+// Display welcome message
+cout << "=======================================" << endl;
+cout << " MATHEMATICAL EXPRESSION EVALUATOR " << endl;
+cout << "=======================================" << endl;
+cout << "This program evaluates mathematical expressions" << endl;
+cout << "using various operators and precedence rules." << endl << endl;
+// Main program code will go here      return 0;
+}
